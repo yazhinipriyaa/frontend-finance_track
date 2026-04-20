4726fbece23092da60156ee3ab87e8aede0676ec
yazhinipriyaa <yazhinirajavel01@gmail.com>
2026-04-20 17:55:10 +0530

    fix netlify build and vite config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 })
